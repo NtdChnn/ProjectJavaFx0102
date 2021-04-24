7362028ba64466acd4754eb496b6716c4b9aebeb
notda <notda@DESKTOP-IRF5H7H>
2021-04-24 18:50:15 +0700

    finish #Beta02

--- src/GameStage/Main.java
+++ src/GameStage/Main.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        GameStage gameStage = new GameStage();
+        GameStage gameStage = new GameStage(); //use this
         System.out.print("type play : ");
         Scanner input_ = new Scanner(System.in);
         String input = input_.nextLine();
         if("play".equals(input))
         {
-            gameStage.main(args);
+            gameStage.main(args); //use this for run GameStage
         }
         
     }
